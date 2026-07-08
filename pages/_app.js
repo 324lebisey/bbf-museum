798565a22d5eb591ac3a6f87927bd1563f80f540
324lebisey <septmoon@naver.com>
2026-07-08 19:13:43 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import './globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
